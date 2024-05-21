57e41ff167ac6403cf8fe02c2ae69e96236a726d
esmesmith03 <esmemcsmith@gmail.com>
2024-05-20 22:09:01 -0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Esme Smith on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
